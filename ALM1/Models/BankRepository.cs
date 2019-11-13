22781f7793f667f56152a69a3dc1e603764163ef
Sara Björnlund <sara_bjornlund@hotmail.com>
2019-11-13 20:20:32 +0100

    Added test for deposit, withdrawl

--- ALM1/Models/BankRepository.cs
+++ ALM1/Models/BankRepository.cs
@@ -7,7 +7,7 @@ namespace ALM1.Models
 {
     public class BankRepository
     {
-        private static List<Customer> Customers = new List<Customer>
+        public List<Customer> Customers = new List<Customer>
         {
             new Customer
             {
@@ -17,8 +17,8 @@ namespace ALM1.Models
                 {
                     new Account
                     {
-                        AccountId = 100101,
-                        Balance = 300000
+                        AccountId = 1,
+                        Balance = 300000m
                     }
                 }
             },
@@ -29,8 +29,8 @@ namespace ALM1.Models
                 Accounts = new List<Account>
                 {
                     new Account {
-                        AccountId = 100102,
-                        Balance = 400000
+                        AccountId = 2,
+                        Balance = 400000m
                     }
                 }
             },
@@ -42,8 +42,8 @@ namespace ALM1.Models
                 {
                     new Account
                     {
-                        AccountId = 100103,
-                        Balance = 500
+                        AccountId = 3,
+                        Balance = 500m
                     }
                 }
             },
@@ -54,25 +54,20 @@ namespace ALM1.Models
             return Customers;
         }
 
-        public string Deposit(int accountId, decimal balance)
+        public void Deposit(int accountId, decimal amount)
         {
-            string message = "Insättning check!";
+            if (amount < 0) throw new Exception();
             var account = Customers.SelectMany(c => c.Accounts).SingleOrDefault(a => a.AccountId == accountId);
 
-            if(account == null)
-            {
-                message = "Kontot finns inte!";
-                return message;
-            }
-            else if (account.Balance < 0)
-            {
-                message = "Kan inte dra ut minus belopp!";
-                return message;
-            }
+            account.Balance += amount;
+        }
 
-            account.Balance += balance;
+        public void Withdrawl(int accountId, decimal amount)
+        {
+            var account = Customers.SelectMany(c => c.Accounts).SingleOrDefault(a => a.AccountId == accountId);
+            if (amount > account.Balance || amount < 0) throw new Exception();
 
-            return message;
+            account.Balance -= amount;
         }
     }
 }
--- TestUppg1/UnitTest1.cs
+++ TestUppg1/UnitTest1.cs
@@ -1,5 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using ALM1.Models;
+using System.Linq;
 
 namespace TestUppg1
 {
@@ -8,8 +10,40 @@ namespace TestUppg1
     {
         [TestMethod]
         [ExpectedException(typeof(ArgumentOutOfRangeException))]
-        public void CanNotDepositUnderBalance()
+        public void HighAmountTest()
         {
+            var model = new BankRepository();
+            model.Withdrawl(1, 53896765);
+        }
+
+        [TestMethod]
+        public void DepositTest()
+        {
+            //Arrange
+            var model = new BankRepository();
+            var account = model.Customers.SelectMany(c => c.Accounts).SingleOrDefault(a => a.AccountId == 1);
+            var balance = account.Balance;
+
+            //Act
+            model.Deposit(account.AccountId, 40m);
+
+            //Assert
+            Assert.AreEqual((balance + 40m), account.Balance);
+        }
+
+        [TestMethod]
+        public void WithdrawlTest()
+        {
+            //Arrange
+            var model = new BankRepository();
+            var account = model.Customers.SelectMany(c => c.Accounts).SingleOrDefault(a => a.AccountId == 1);
+            var balance = account.Balance;
+
+            //Act
+            model.Withdrawl(account.AccountId, 50m);
+
+            //Asset
+            Assert.AreEqual((balance - 50m), account.Balance);
         }
     }
 }

c472fd30e549ca9026b36f59c37ff7d18f6a923d
Sara Björnlund <sara_bjornlund@hotmail.com>
2019-11-13 14:24:59 +0100

    Added customer list with accounts

---
+++ ALM1/Models/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ALM1.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+
+        public List<Account> Accounts { get; set; }
+    }
+}
